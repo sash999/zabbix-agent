dcf4826a1f87a829f71628db25a6dfa360cbde44
sash999 <sash999@yandex.ru>
2019-03-28 12:18:32 +0300

    Переписан обработчик для работы с zabbix v.4

--- zabbix-agent.ino
+++ zabbix-agent.ino
@@ -7,6 +7,8 @@
 #include <OneWire.h>
 #include <SoftwareSerial.h>
 
+
+
 // Описываем датчик температуры DS18B20
 #define POWER_MODE  0 // режим питания ds18b20, 0 - внешнее, 1 - паразитное
 OneWire sensDs (6);  // датчик подключен к выводу 6
@@ -52,9 +54,8 @@ void setup() {
   Ethernet.begin(mac, ip);
   server.begin();
   GetSensorsData(temperature_am, temperature_bmp, temperature_ds, humidity, pressure, co2);
-//  DisplayOnLCD(temperature_ds, temperature_am, humidity, pressure);
   DisplayOnLCD(temperature_ds, temperature_am, humidity, co2);
- 
+  //Serial.println("startup complete");
  
 }
 
@@ -123,99 +124,111 @@ void DisplayOnLCD(float &t_in, float &t_room, float &h, unsigned int &co2) {
 
 void GetSensorsData(float &t_am , float &t_bmp, float &t_ds, float &h, float &p, unsigned int &co2) {
   th_sensor.Read();
-  t_am = th_sensor.cTemp;
-  h = th_sensor.Humidity;
-  t_ds = Get_DS18B20_Temp();
-  t_bmp = bmp.readTemperature();
-  p = bmp.readPressure()/133.3;
-  co2 = Get_MHZ19_data();
+  t_am = th_sensor.cTemp;            //температура AM2320
+  t_ds = Get_DS18B20_Temp();         //температура DS18B20
+  t_bmp = bmp.readTemperature();     //температура BMP180
+  h = th_sensor.Humidity;            //влажность (AM2320)
+  p = bmp.readPressure()/133.3;      //давление (BMP180)
+  co2 = Get_MHZ19_data();            //содержание СО2 (MH-Z19)
 }
 
 void loop() {
 
-  String readString = String(20); 
+
   
   if (millis() - timestamp < 0) timestamp=millis(); /* проверка на переход millis через 0 */
   if (millis() - timestamp > UPDATE_INTERVAL) {
     timestamp = millis();
     GetSensorsData(temperature_am, temperature_bmp, temperature_ds, humidity, pressure, co2);
-//    DisplayOnLCD(temperature_ds, temperature_am, humidity, pressure);
     DisplayOnLCD(temperature_ds, temperature_am, humidity, co2);
   }
   
-  readString = "";
-  
+
+  size_t size;
+  unsigned int i;
+  unsigned int datalen;
+  char request[20];
   if (EthernetClient client = server.available())
   {
-    while (client.connected()) {
-      if (client.available()) {
-               
-        char c = client.read();
-        if (c == '\n') // end of query from zabbix server 
+
+   while((size = client.available()) > 0)
         {
-          client.print("ZBXD\x01"); // response header
+          uint8_t* msg = (uint8_t*)malloc(size+1);
+          memset(msg, 0, size+1);
+          size = client.read(msg,size);
           
-          if (readString == "agent.ping") {
+          //Serial.println("received request");
+          datalen = msg[5];  //длина данных в 5-м байте пакета
+          //Serial.print("data length is ");
+          //Serial.println(datalen);
+
+          for (i=0; i<=datalen; i++) {
+            request[i] = msg[13 + i];  //запрос начиная с 13-го байта
+          }
+         free(msg);  
+        }
+         
+          //Serial.print("request is ");
+          //Serial.println(request);
+
+                   
+           
+          if (strcmp(request,"agent.ping") == 0) {
+            client.print("ZBXD\x01"); // response header   
             byte responseBytes [] = {0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, '1'}; 
             client.write(responseBytes, 9);
-          } else if (readString == "env.temp") {
 
+          } else if (strcmp(request,"env.co2_ppm") == 0) {
+            client.print("ZBXD\x01"); // response header   
+            byte responseBytes [] = {(byte) String(co2).length(), 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+            client.write(responseBytes, 8);
+            client.print(co2);
+                  
+          } else if (strcmp(request,"env.temp") == 0) {
+            client.print("ZBXD\x01"); // response header   
             byte responseBytes [] = {(byte) String(temperature_am).length(), 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
             client.write(responseBytes, 8);
             client.print(temperature_am);
            
             
-          } else if (readString == "env.humidity") {
-
+          } else if (strcmp(request,"env.humidity") == 0 ) {
+            client.print("ZBXD\x01"); // response header   
             byte responseBytes [] = {(byte) String(humidity).length(), 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
             client.write(responseBytes, 8);
             client.print(humidity);
             
             
-          } else if (readString == "env.pressure") {
-
-            
+          } else if (strcmp(request,"env.pressure") == 0 ) {
+            client.print("ZBXD\x01"); // response header               
             byte responseBytes [] = {(byte) String(pressure).length(), 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
             client.write(responseBytes, 8);
             client.print(pressure);
            
             
-          } else if (readString == "env.temp_bmp") {
-
+          } else if (strcmp(request,"env.temp_bmp") == 0 ) {
+            client.print("ZBXD\x01"); // response header   
             byte responseBytes [] = {(byte) String(temperature_bmp).length(), 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
             client.write(responseBytes, 8);
             client.print(temperature_bmp);
             
             
-            } else if (readString == "env.temp_ds") {
+           } else if (strcmp(request,"env.temp_ds") == 0 ) {
+             client.print("ZBXD\x01"); // response header   
+             byte responseBytes [] = {(byte) String(temperature_ds).length(), 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+             client.write(responseBytes, 8);
+             client.print(temperature_ds);
 
-            
-            byte responseBytes [] = {(byte) String(temperature_ds).length(), 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-            client.write(responseBytes, 8);
-            client.print(temperature_ds);
-
-             } else if (readString == "env.co2_ppm") {
-
-            
-            byte responseBytes [] = {(byte) String(co2).length(), 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-            client.write(responseBytes, 8);
-            client.print(co2);
-            
           
-          } else {
-            byte responseBytes [] = {0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-            client.write(responseBytes, 8);
-            client.print("ZBX_NOTSUPPORTED");
-           
+           } else {
+             client.print("ZBXD\x01"); // response header   
+             byte responseBytes [] = {0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+             client.write(responseBytes, 8);
+             client.print("ZBX_NOTSUPPORTED");
           }
-          break;
+                
+          delay(10);
+          client.stop();
         }
-        else if (readString.length() < 20) {
-          readString = readString + c;
-        }
-      }
-    }
-    delay(10);
-    client.stop();
+ 
+    
   }
-}
